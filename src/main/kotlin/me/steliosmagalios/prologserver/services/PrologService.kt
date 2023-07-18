39daa8659f7757a0852137a14ac1c479b9f99793
Stelios Magalios <47584641+steliosmagalios@users.noreply.github.com>
2023-07-18 14:06:50 +0300

    prolog executes

--- src/main/kotlin/me/steliosmagalios/prologserver/services/PrologService.kt
+++ src/main/kotlin/me/steliosmagalios/prologserver/services/PrologService.kt
@@ -1,10 +1,19 @@
 package me.steliosmagalios.prologserver.services
 
 import org.springframework.stereotype.Service
+import com.parctechnologies.eclipse.EclipseEngine;
+import com.parctechnologies.eclipse.EclipseEngineOptions
+import com.parctechnologies.eclipse.OutOfProcessEclipse
 
 @Service
 class PrologService {
 
-    fun execute() = "Executed!"
+    private val engine: EclipseEngine = OutOfProcessEclipse(EclipseEngineOptions())
+
+    fun execute(): String {
+        val result = engine.rpc("X = [1, 2, 3]")
+
+        return result.arg(1).toString()
+    }
 
 }
